c5cc230b029ef8ab4f473e980e865f4522ce5d7b
Taras Mankovski <taras@quandl.com>
2014-09-22 14:41:59 -0400

    Removed validation export

--- addon/macros.js
+++ addon/macros.js
@@ -1,11 +1,9 @@
 import controller from './macros/controller';
 import arrayController from './macros/array-controller';
 import objectController from './macros/object-controller';
-import validation from './macros/validation';
 
 export {
   controller,
   arrayController,
-  objectController,
-  validation
+  objectController
   };
